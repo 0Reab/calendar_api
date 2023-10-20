c03fe8d4ff7eeed220cb48a1a2189ac7e844017f
0Reab <mihajlofrancic@gmail.com>
2023-10-20 16:47:20 +0200

    xd

--- main.py
+++ main.py
@@ -8,3 +8,29 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+
+SCOPES = ["https://www.googleapis.com/auth/calendar"]
+
+def main():
+    creds = None
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json")
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build("calendar", "v3", credentials = creds)
+        now = datetime.datetime.now().isoformat() + "Z"
+        event_result = service.events().list(calendarId="primary", timeMin="now", maxResults=10, singleEvents=True, orderBy="startTime")
+        events = event_result.get("items", [])
+
+    except HttpError as error:
+        print("Error occurred:", error)
