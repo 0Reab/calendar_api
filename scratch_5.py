9c9634d820268c094ace0cd3e02db578abcefc25
0Reab <mihajlofrancic@gmail.com>
2023-10-27 15:05:18 +0200

    xd

--- scratch_5.py
+++ scratch_5.py
@@ -3,34 +3,35 @@ file = open('chess.txt','w')
 running = True
 
 while running:
-    print("1 - time"
-          "2 - my tactic"
-          "3 - opponent tactic"
-          "4 - endgame"
-          "5 - opening"
-          "6 - queen hang"
-          "7 - free piece"
-          "8 - bad calculation"
-          "9 - failed attack")
-    if input("1"):
+    print("1 - time\n"
+          "2 - my tactic\n"
+          "3 - opponent tactic\n"
+          "4 - endgame\n"
+          "5 - opening\n"
+          "6 - queen hang\n"
+          "7 - free piece\n"
+          "8 - bad calculation\n"
+          "9 - failed attack\n"
+          "q - SAVE & QUIT\n")
+    if input()== "1":
         mistakes.append("time")
-    if input("2"):
+    if input()== "2":
         mistakes.append("my tactic")
-    if input("3"):
+    if input()== "3":
         mistakes.append("opponent tactic")
-    if input("4"):
+    if input()== "4":
         mistakes.append("endgame")
-    if input("5"):
+    if input()== "5":
         mistakes.append("opening")
-    if input("6"):
+    if input()== "6":
         mistakes.append("queen hang")
-    if input("7"):
+    if input()== "7":
         mistakes.append("free piece")
-    if input("8"):
+    if input()== "8":
         mistakes.append("bad calculation")
-    if input("9"):
+    if input()== "9":
         mistakes.append("failed attack")
-    if input("q"):
+    if input()== "q":
         for i in mistakes:
             file.write(i + "\n")
         file.close()
